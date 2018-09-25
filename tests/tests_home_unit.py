98d5e42753e0119e7db77e443d96ce4e5b68cb97
clement-daubrenet <clement-daubrenet@users.noreply.github.com>
2018-09-25 11:10:39 +0100

    Update tests

--- tests/tests_home_unit.py
+++ tests/tests_home_unit.py
@@ -1,23 +1,47 @@
 from api.home.views import HomeHandler
-from unittest.mock import patch
-import uuid
+from unittest.mock import patch, Mock
 
 
-def test_get_most_common_words_empty():
+def test_get_most_common_words():
+    """
+    Testing the right behavior of get_most_common_words.
+    """
+    result = HomeHandler._get_most_common_words(['business',
+                                                 'article',
+                                                 'stuff',
+                                                 'business',
+                                                 'article'], 10)
+    assert result == [('business', 2), ('article', 2), ('stuff', 1)]
+
+
+def test_get_most_common_words_upper_case_lower_case():
+    """
+    Testing lower case/upper case mix up: they should be counted as the same
+    word in lower case.
+    """
+    result = HomeHandler._get_most_common_words(['Business',
+                                                 'business',
+                                                 'bUsiness',
+                                                 'busineSS'], 10)
+    assert result == [('business', 4)]
+
+
+def test_get_most_common_words_edge_case_1():
+    """
+    Testing 1 element scenario.
+    :return:
+    """
+    result = HomeHandler._get_most_common_words(['business'], 10)
+    assert result == [('business', 1)]
+
+
+def test_get_most_common_words_edge_case_2():
     """
     Testing empty scenario.
     :return:
     """
-    result = HomeHandler._get_most_common_words([],10)
+    result = HomeHandler._get_most_common_words([], 10)
     assert result == []
-
-
-def test_get_most_common_words_regular():
-    """
-    Testing the right behavior of get_most_common_words.
-    """
-    result = HomeHandler._get_most_common_words(['business', 'article'], 10)
-    assert result == [('business', 1), ('article', 1)]
 
 
 def test_scale_frequencies():
@@ -31,6 +55,24 @@ def test_scale_frequencies():
                       ('sport', 8),
                       ('technology', 6),
                       ('others', 1)]
+
+
+def test_scale_frequencies_edge_case_1():
+    """
+    Testing the frequency scaling for the front-end.
+    :return:
+    """
+    result = HomeHandler._scale_frequencies([('business', 15)])
+    assert result == [('business', 10)]
+
+
+def test_scale_frequencies_edge_case_2():
+    """
+    Testing the frequency scaling for the front-end.
+    :return:
+    """
+    result = HomeHandler._scale_frequencies([])
+    assert result == []
 
 
 @patch('api.home.views.session.add')
@@ -56,7 +98,7 @@ def test_update_word(query_mock, commit_mock):
     :param commit_mock: A database "commit" operation mock
     :return:
     """
-    word_id = uuid.uuid4()
+    word_id = Mock()
     new_count = 3
     HomeHandler._update_word(word_id, new_count)
     assert query_mock.called is True
