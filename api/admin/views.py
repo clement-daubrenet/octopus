1df7e4996aa76e234d61d2797141585478f138b4
clement-daubrenet <clement-daubrenet@users.noreply.github.com>
2018-10-03 00:46:21 +0100

    Remove pagination

--- api/admin/views.py
+++ api/admin/views.py
@@ -9,20 +9,14 @@ class AdminHandler(RequestHandler):
     """
     This class is in charge of handling the admin page.
     """
-    def get(self, per_page=10):
+    def get(self):
         """
         Getting the admin page.
-        :param per_page: the number of items per page.
         :return:
         """
-        page = self.get_argument("page", 1)
-        count = session.query(Word).count()
-
-        ordered_words = \
-            session.query(Word).order_by(desc('count'))\
-            .limit(per_page).offset((page - 1) * per_page)
-
-        pagination = Pagination(page, per_page, count)
+        ordered_words = session.query(Word)\
+            .order_by(desc('count')).all()
+        
         self.render('admin.html',
-                    ordered_words=ordered_words,
-                    pagination=pagination)
+                    ordered_words=ordered_words)
+
